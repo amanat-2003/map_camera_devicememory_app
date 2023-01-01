ab0db0ead53cf0b1d78769996318ec80c6c22316
Amanat Singh <amanatsinghnain@gmail.com>
2023-01-01 14:19:24 +0530

    lec303 and 304 completed

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -22,6 +23,24 @@ class GreatPlaces with ChangeNotifier {
       locaion: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((map) => Place(
+              id: map['id'],
+              title: map['title'],
+              image: File(map['image']),
+              locaion: null,
+            ))
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -19,34 +19,43 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Got no places yet, start adding some!'),
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-                },
-                icon: Icon(Icons.add),
-                label: Text('Add A New Place'),
-              )
-            ],
-          ),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                      backgroundImage: FileImage(greatPlaces.items[i].image)),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    // Go To Detail Page...
-                  },
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Got no places yet, start adding some!'),
+                      TextButton.icon(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed(AddPlaceScreen.routeName);
+                        },
+                        icon: Icon(Icons.add),
+                        label: Text('Add A New Place'),
+                      )
+                    ],
+                  ),
                 ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                              backgroundImage:
+                                  FileImage(greatPlaces.items[i].image)),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go To Detail Page...
+                          },
+                        ),
+                      ),
               ),
       ),
     );
